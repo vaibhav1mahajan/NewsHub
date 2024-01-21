d8831275890b99f46f89ef5885ceacbc3a559dbd
vaibhav1mahajan <vaibhavmahajan2257@gmail.com>
2024-01-21 19:22:38 +0530

    project completed till 32

--- src/components/News.js
+++ src/components/News.js
@@ -68,7 +68,7 @@ export class News extends Component {
         <div className="row">
         { !this.state.loading && this.state.articles.map((element)=>{
           return    <div className="col-md-4 my-3" key={element.url}>
-          <NewsItem title={element.title.slice(0,40)} description={element.description===null?"":element.description.slice(0,80)} imageUrl={element.urlToImage} newsUrl={element.url}></NewsItem>
+          <NewsItem title={element.title.slice(0,40)} description={element.description===null?"":element.description.slice(0,80)} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}></NewsItem>
           </div>
         })}
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 
-
 export class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl,newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div>
         <div className="card">
-          <img src={imageUrl===null?'https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg':imageUrl} className="card-img-top" alt="..." />
+          <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{left:"88%", zIndex:"1"}}>
+            {source}
+            <span class="visually-hidden">unread messages</span>
+          </span>
+          <img
+            src={
+              imageUrl === null
+                ? "https://t3.ftcdn.net/jpg/03/27/55/60/360_F_327556002_99c7QmZmwocLwF7ywQ68ChZaBry1DbtD.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
+            <h5 className="card-title">{title}... </h5>
+            <p className="card-text">{description}...</p>
             <p className="card-text">
-             {description}...
+              <small class="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toUTCString()}
+              </small>
             </p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-dark">
               Read More
